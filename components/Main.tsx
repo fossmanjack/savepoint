44e40576078789d2baa8a27e2d9a119a699cde26
Jack <mjmeyers@proton.me>
2023-03-02 13:48:19 -0600

    Began scaffolding Main

---
+++ components/Main.tsx
@@ -0,0 +1,29 @@
+// Main.tsx
+// Handles all displayed information
+// jsp/P3Soft
+// 2023-03-02
+
+// React Native imports
+import { useState } from 'react';
+import {
+	Modal,
+	Text,
+	TextInput,
+	View
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
+
+// Community imports
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, Icon } from '@rneui/themed';
+
+// Screens
+import TimelineScreen from '../screens/TimelineScreen';
+import RolodexScreen from '../screens/RolodexScreen';
+import QuizScreen from '../screens/QuizScreen';
+import DevScreen from '../screens/DevScreen';
+
+// Assets and utils
+
